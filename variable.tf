fb272b469560f687d125518ee566a95ee7b4af28
Gomex <linux.rafa@gmail.com>
2020-08-29 14:25:53 -0300

    Preciso dessas duas novas variáveis

--- variable.tf
+++ variable.tf
@@ -2,9 +2,13 @@ variable "hash_commit" {
   default = "806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
 }
 
-variable "name" {
+variable "app_name" {
   type        = string
-  default     = "Testando interpolação"
-  description = ""
+  default     = "Sem nome"
+  description = "Nome da app"
 }
 
+variable "instance_type" {
+  type = string
+  description = "Tipo de instância ec2 usada na AWS"
+}
