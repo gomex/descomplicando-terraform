a7fe2dfa546461ecafa3f710276e2e3ac4c587d3
Gomex <linux.rafa@gmail.com>
2020-08-29 15:05:57 -0300

    Preciso de duas máquinas

--- terrafile.tf
+++ terrafile.tf
@@ -7,6 +7,7 @@ module "ec2" {
   source                  = "git@github.com:gomex/descomplicando-terraform-modulo.git?ref=v0.2"
   app_name                = "turma3"
   instance_type           = "t3.micro"
+  count                   = 2
 }
 
 output "ip_address_ec2" {
